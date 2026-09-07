f5c7d8a77223be92f2b2017ea184ead0d426c317
miguelbarrigana-png <miguelbarrigana@gmail.com>
2026-09-07 12:41:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 // Without it, phones can keep showing an old version indefinitely.
 // (The cache key name itself is an internal identifier, left as-is for
 // upgrade compatibility with installs already running v5.)
-var CACHE_NAME = 'contas-em-dia-v26';
+var CACHE_NAME = 'contas-em-dia-v27';
 var SHELL_FILES = [
   './',
   './index.html',
@@ -85,3 +85,4 @@ self.addEventListener('fetch', function (event) {
     );
   }
 });
+ 
