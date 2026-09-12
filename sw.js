cecafcb98ea373956fa6a935bce08ce5228c750c
miguelbarrigana-png <miguelbarrigana@gmail.com>
2026-09-12 21:11:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,11 +8,13 @@
 // Without it, phones can keep showing an old version indefinitely.
 // (The cache key name itself is an internal identifier, left as-is for
 // upgrade compatibility with installs already running v5.)
-var CACHE_NAME = 'contas-em-dia-v52';
+var CACHE_NAME = 'contas-em-dia-v53';
 var SHELL_FILES = [
   './',
   './index.html',
   './manifest.json',
+  './alerts-engine.js',
+  './alerts-store.js',
   './push-config.js',
   './push-policy.js',
   './push-client.js',
